35a8ea29e0498b7c6ca75905cf2ca6383c26d16a
wittyking <32215818+wittyking@users.noreply.github.com>
2017-09-25 11:27:44 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'eXUCLEtyEvkdI1gCWqEHIKmjtIUUfGIQZEHe2shkANNExaOCzKP8EHqAAsCjpMfgI9o0TSrrC1JHfm/uhu9NskwMzZ76qJLpcpg1ol3Wssc1IH8Wr1z1ljogbyoDeNY4c2wvgnneIU8i49RGkZK9SQdB04t89/1O/w1cDnyilFU=';
+$access_token = 'eXUCLEtyEvkdI1gCWqE0TSrrC1JHfm/uhu9NskwMzZ76qJLpcpg1ol3Wssc1IH8Wr1z1ljogbyoDeNY4c2wvgnneIU8i49RGkZK9SQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
